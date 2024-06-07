45ddf05110d62365101f616e78d6e252e1c01b09
zakky <sadaoza@gmail.com>
2024-06-07 21:52:27 +0900

    useState study

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { ColorfulMeessage } from "./components/ColorfulMessage";
 
 export const App = () => {
-    const onClickBtn = () => alert('Hello');
+    const [num, setNum] = useState(0);
+    const onClickCountup = () => {
+        setNum(num + 1);
+    };
     return (
       <>
         <h1 style={{ color: "red" }}>こんにちは</h1>
         <ColorfulMeessage color="blue" >お元気ですか？</ColorfulMeessage>
         <ColorfulMeessage color="green">元気です!</ColorfulMeessage>
-        <button onClick={onClickBtn}>ボタン</button>
+        <button onClick={onClickCountup}>カウントアップ</button>
+        <p>{num}</p>
       </>
     );
   }
